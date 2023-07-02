82f80ffc4fe7c48eb497fecca67d6e4de2d6c06d
Noura <edcarlos4844@gmail.com>
2023-07-02 14:06:09 -0300

    First unitary test

--- src/services/register.test.ts
+++ src/services/register.test.ts
@@ -1,5 +1,33 @@
-import { test, expect } from 'vitest'
+import { it, describe, expect } from 'vitest'
+import { RegisterService } from './register'
+import { compare } from 'bcryptjs'
 
-test('should be able to sum 2+2', () => {
-  expect(2 + 2).toBe(4)
+describe('Register service', () => {
+  it('should hash user password upon registration', async () => {
+    const registerService = new RegisterService({
+      async findByEmail(email) {
+        return null
+      },
+
+      async create(data) {
+        return {
+          id: 'test user',
+          name: data.name,
+          email: data.email,
+          password_hash: data.password_hash,
+          created_at: new Date(),
+        }
+      },
+    })
+
+    const { user } = await registerService.execute({
+      name: 'João predo',
+      email: 'joao@predo.com',
+      password: '123456',
+    })
+
+    const isHashed = await compare('123456', user.password_hash)
+
+    expect(isHashed).toBe(true)
+  })
 })
